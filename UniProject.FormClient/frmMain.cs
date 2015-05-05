01c4c2f8552dff5b439dfbc13347046eb67fbe0b
Dan Scott <me@idanscott.co.uk>
2015-05-05 10:37:59 +0100

    Client update

--- UniProject.FormClient/frmMain.cs
+++ UniProject.FormClient/frmMain.cs
@@ -27,7 +27,7 @@ namespace UniProject.FormClient
         public frmMain()
         {
             InitializeComponent();
-            m_Client = new Client("141.163.233.115", 101);
+            m_Client = new Client("141.163.83.82", 101);
             m_ShouldWork = true;
             m_ScreenWorkerThread = new Thread(ScreenFeed);
             m_Client.ClientConnected += client_ClientConnected;
